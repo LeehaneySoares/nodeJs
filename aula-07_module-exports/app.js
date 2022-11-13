c263a578a4228b35d09f8f996a3b2db53b12997a
Leehaney Soares <leehaney.soares@gmail.com>
2022-11-13 20:02:28 -0300

    learn(nodeJS): aprendendo a remover, instalar e atualizar as dependencias

--- aula-07_module-exports/app.js
+++ aula-07_module-exports/app.js
@@ -5,5 +5,9 @@
 
 // console.log(p1.nomeCompleto())
 
+const path = require('path') // Função nativa do NodeJs para manipular caminhos de pastas e arquivos
+
 console.log(__dirname) // Me retorna o caminho absoluto da PASTA
 console.log(__filename) // Me retorna o caminho absoluto do ARQUIVO atual.
+
+console.log(path.resolve(__filename, 'app.js'))
