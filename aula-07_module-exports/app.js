de8a3730e446686513e9ef2b5aa6f857452c9129
Leehaney Soares <leehaney.soares@gmail.com>
2022-11-13 14:35:27 -0300

    learn(nodeJs): aprendendo a utilizar o dirname e o filename

--- aula-07_module-exports/app.js
+++ aula-07_module-exports/app.js
@@ -1,6 +1,9 @@
-const module1 = require('./mod')
-const Pessoa = module1.Pessoa
+// const module1 = require('./mod')
+// const Pessoa = module1.Pessoa
 
-const p1 = new Pessoa('Leehaney', 'Soares', 25)
+// const p1 = new Pessoa('Leehaney', 'Soares', 25)
 
-console.log(p1.nomeCompleto())
+// console.log(p1.nomeCompleto())
+
+console.log(__dirname) // Me retorna o caminho absoluto da PASTA
+console.log(__filename) // Me retorna o caminho absoluto do ARQUIVO atual.
