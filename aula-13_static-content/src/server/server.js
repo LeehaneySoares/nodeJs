8222e83b36644981879b6e67a920cb5c4034336c
Leehaney Soares <leehaney.soares@gmail.com>
2023-02-22 09:06:41 -0300

    learn(nodeJs): fazendo com que o express utilize arquivos estáticos da minha pasta public

--- aula-13_static-content/src/server/server.js
+++ aula-13_static-content/src/server/server.js
@@ -4,6 +4,8 @@ const app = express()
 const routes = require('../routes/routes')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.resolve(__dirname, '..', '..', 'public')))
+
 app.set('views', path.resolve(__dirname, '..', 'views'))
 app.set('view engine', 'ejs')
 
